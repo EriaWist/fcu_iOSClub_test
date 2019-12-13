262d0175c85c3c992287cabfa5a8f5750234c9b7
Eria <c121680118@gmail.com>
2019-12-13 21:56:07 +0800

    first

---
+++ fcu_iOSClub_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fcu_iOSClub_test
+//
+//  Created by 阿騰 on 2019/12/13.
+//  Copyright © 2019 阿騰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fcu_iOSClub_testTests/fcu_iOSClub_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  fcu_iOSClub_testTests.swift
+//  fcu_iOSClub_testTests
+//
+//  Created by 阿騰 on 2019/12/13.
+//  Copyright © 2019 阿騰. All rights reserved.
+//
+
+import XCTest
+@testable import fcu_iOSClub_test
+
+class fcu_iOSClub_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
